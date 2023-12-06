c0feacb2b6b62c155352e457f56248507181aee9
Manil Maharjan <manil.maharjan07@gmail.com>
2023-12-06 08:47:06 +0545

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using DependencyInjection_Service_LifeTime.Models;
+using DependencyInjection_Service_LifeTime.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text;
+
+namespace DependencyInjection_Service_LifeTime.Controllers
+{
+    public class HomeController : Controller
+    {
+        /*private readonly ILogger<HomeController> _logger;*/
+
+        private readonly ITransientGuidService _transient1;
+        private readonly ITransientGuidService _transient2;
+
+        private readonly IScopedGuidService _scoped1;
+        private readonly IScopedGuidService _scoped2;
+
+        private readonly ISingletonGuidService _singleton1;
+        private readonly ISingletonGuidService _singleton2;
+
+        public HomeController(
+            ITransientGuidService transient1,
+            ITransientGuidService transient2,
+
+            IScopedGuidService scoped1,
+            IScopedGuidService scoped2,
+
+            ISingletonGuidService singleton1,
+            ISingletonGuidService singleton2
+
+            )
+        {
+            _transient1 = transient1;
+            _transient2 = transient2;
+            _scoped1 = scoped1; 
+            _scoped2 = scoped2;
+            _singleton1 = singleton1;
+            _singleton2 = singleton2;
+        }
+
+        public IActionResult Index()
+        {
+            StringBuilder msg = new StringBuilder();
+
+            msg.Append($"Transient 1: { _transient1.GetGuid()}\n");
+            msg.Append($"Transient 2: { _transient2.GetGuid()}\n");
+
+            msg.Append($"Scoped 1: { _scoped1.GetGuid()}\n");
+            msg.Append($"Scoped 2: {_scoped2.GetGuid()}\n");
+            
+            msg.Append($"Singleton 1: {_singleton1.GetGuid()}\n");
+            msg.Append($"Singleton 2: {_singleton2.GetGuid()}\n");
+            
+            return Ok(msg.ToString());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Services/SingletonGuidService.cs
@@ -0,0 +1,16 @@
+﻿namespace DependencyInjection_Service_LifeTime.Services
+{
+    public class SingletonGuidService : ISingletonGuidService
+    {
+        private readonly Guid Id;
+        public SingletonGuidService()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public string GetGuid()
+        {
+            return Id.ToString();
+        }
+    }
+}
